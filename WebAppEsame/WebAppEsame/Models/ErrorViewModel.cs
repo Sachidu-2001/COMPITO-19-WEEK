e5a9fd9a8b36257d9b51bf5ebae17d53f5be29c8
Sachidu-2001 <fsachi0001@gmail.com>
2025-03-28 11:58:43 +0100

    commit

---
+++ WebAppEsame/WebAppEsame/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppEsame.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
